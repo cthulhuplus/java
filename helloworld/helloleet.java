146857ab552638011716d72bdc4f9ed181701b35
cthulhuplus <brookswm@gmail.com>
2017-06-30 14:02:21 -0400

    Created helloleet

---
+++ helloworld/helloleet.java
@@ -0,0 +1,11 @@
+public class helloleet {
+  public static void main(String[] args) {
+    byte three = 3;
+    byte one = 1;
+    byte zero = 0;
+    float a = 2.0f;
+    boolean b = true;
+    String output = "H" + three + one + one + zero + " " + "w" + zero + "r" + one + "d" + " " + a + " " + b;
+    System.out.println(output);
+  }
+}
